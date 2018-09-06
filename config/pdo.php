449cdb8f4cee26cbd62d747d273063946ab19503
abhi9121998 <38099265+abhi9121998@users.noreply.github.com>
2018-09-06 23:39:20 +0530

    Add files via upload
    config files

--- config/pdo.php
+++ config/pdo.php
@@ -1,1 +1,15 @@
-
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "123";
+$dbname="super_user";
+try {
+    $DB=new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
